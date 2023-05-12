bdb3089b127ebaf77c585390b3bba5116a08b8f8
sikara <987555458@qq.com>
2023-05-12 17:07:06 +0800

    fix 部署3

--- scripts/tsconfig-paths-bootstrap.js
+++
@@ -1,9 +0,0 @@
-const tsConfigPaths = require("tsconfig-paths");
-
-const baseUrl = "./";
-const { paths } = require("../tsconfig.json").compilerOptions;
-
-tsConfigPaths.register({
-  baseUrl,
-  paths,
-});
