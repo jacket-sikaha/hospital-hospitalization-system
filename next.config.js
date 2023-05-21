fd266ea9192c12127f0c7d1007f6ef65b9c50411
sikaha <77327970+jacket-sikaha@users.noreply.github.com>
2023-05-21 15:15:09 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,20 +9,20 @@ const nextConfig = (phase) => {
     // 在 Next.js 中使用 antd-mobile 需要做一些额外的配置
     transpilePackages: ["antd-mobile"],
     // reactStrictMode: true,
-    env: {
-      mongodb_username: "sikara",
-      mongodb_password: "sikara",
-      mongodb_clustername: "cluster0",
-      mongodb_database: "my-site-dev",
-      MONGODB_HOST: "xxxxx",
-      MONGODB_PORT: "xxxx",
-      SMTP_USER: "xxxx",
-      SMTP_PASSWORD: "xxxxx",
-      SMTP_HOST: "smtp.qq.com", //'smtp.163.com' 163 email
-      SMTP_PORT: 587,
-      EMAIL_FROM: "xxxxx",
-      SECRET: "sikara",
-    },
+//     env: {
+//       mongodb_username: "sikara",
+//       mongodb_password: "sikara",
+//       mongodb_clustername: "cluster0",
+//       mongodb_database: "my-site-dev",
+//       MONGODB_HOST: "xxxxx",
+//       MONGODB_PORT: "xxxx",
+//       SMTP_USER: "xxxx",
+//       SMTP_PASSWORD: "xxxxx",
+//       SMTP_HOST: "smtp.qq.com", //'smtp.163.com' 163 email
+//       SMTP_PORT: 587,
+//       EMAIL_FROM: "xxxxx",
+//       SECRET: "sikara",
+//     },
     typescript: {
       // !! WARN !!
       // Dangerously allow production builds to successfully complete even if
